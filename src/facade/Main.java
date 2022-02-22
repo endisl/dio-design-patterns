93d40a8e19e7c0f76210092c9a3025e51ccd0c15
endisl <endiluamba@gmail.com>
2022-02-21 21:34:06 -0300

    Developed a Notification Service using the Facade

--- src/facade/Main.java
+++ src/facade/Main.java
@@ -3,11 +3,7 @@ package facade;
 public class Main {
 
     public static void main(String[] args) {
-        var server = new NotificationServer();
-        var connection = server.connect("ip");
-        var authToken = server.authenticate("appID", "key");
-        var message = new Message("Hello World");
-        server.send(authToken, message, "target");
-        connection.disconnect();
+        var service = new NotificationService();
+        service.send("Hello World", "target");
     }
 }
